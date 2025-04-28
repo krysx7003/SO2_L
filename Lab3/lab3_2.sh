19b88d7686a91f2179011d2d2559cf497bcb2ecd
krysx7003 <krysx7003@gmail.com>
2025-04-28 22:46:09 +0200

    Lab3 ver2

--- Lab3/lab3_2.sh
+++ Lab3/lab3_2.sh
@@ -4,7 +4,8 @@ cd ../../../../../../dev
 echo $PWD
 
 cat urandom |
-    head -c 40 |
-    tr a-z A-Z | tr -cd A-Z > ../home/napnap/Studia/SO2/SO2_L/Lab3/Wynik.txt 
+    tr a-z A-Z |
+    tr -cd 0-9 |
+    head -c 7 > ../home/napnap/Studia/SO2/SO2_L/Lab3/Wynik.txt 
 
 echo
